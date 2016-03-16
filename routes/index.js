6c4adab349143fc357fe9756e74beed17982e57c
Dishant Chavda <dishantchavda@gmail.com>
2016-03-16 13:41:07 +0000

    completing all functionality

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,29 @@ router.get('/', function(req, res, next) {
 
 // Process date view
 router.get('/:date', function(req, res, next) {
-  res.end(JSON.stringify({date:req.params.date}));
+    var monthNames = [
+          "January", "February", "March",
+          "April", "May", "June", "July",
+          "August", "September", "October",
+          "November", "December"
+        ];
+    var inDate = req.params.date;
+    var result = {
+        "unix":null,
+        "natural": null,
+    };
+    var naturalCheck = new Date(inDate);
+    if(!isNaN(inDate) && inDate.length == 10){
+        // its unix date convert to natural
+        result.unix = inDate;
+        var temp = new Date(parseInt(inDate,10) * 1000);
+        result.natural = monthNames[temp.getMonth()] + " " + temp.getDate() + ", " + temp.getFullYear();
+    } else if( naturalCheck.toDateString() != "Invalid Date" && isNaN(inDate)){
+        // natural date given
+        result.natural = inDate;
+        result.unix = naturalCheck.getTime()/1000;
+    }
+    res.end(JSON.stringify(result));
 });
 
 module.exports = router;
